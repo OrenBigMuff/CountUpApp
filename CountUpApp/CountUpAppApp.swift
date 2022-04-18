16338feae56e948611a1714e806733992c675a37
oinarhythm <oinarhythm0508@gmail.com>
2022-04-18 23:42:13 +0900

    Initial Commit

---
+++ CountUpApp/CountUpAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountUpAppApp.swift
+//  CountUpApp
+//
+//  Created by 今井暖 on 2022/04/18.
+//
+
+import SwiftUI
+
+@main
+struct CountUpAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
